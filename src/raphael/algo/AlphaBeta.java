f82f402ea84e7ca59da1c52223abf90219cab77b
Raphael <raphael.garbarg@gmail.com>
2020-03-08 18:27:14 +0100

    mini amélioration du AB

--- src/raphael/algo/AlphaBeta.java
+++ src/raphael/algo/AlphaBeta.java
@@ -1,5 +1,10 @@
 package raphael.algo;
 
+import java.util.List;
+
+import raphael.jeu.Etat;
+import raphael.jeu.Utilitaire;
+
 public class AlphaBeta implements Algorithme{
 	private static Noeud bon;
 	private static Joueur joueur;
@@ -20,11 +25,29 @@ public class AlphaBeta implements Algorithme{
 			return noeud.evaluation(joueur);
 		calc++;
 		
+		List<Noeud> succ = noeud.successeurs();
+		if(profondeur > 1) {
+			succ.sort((o1, o2) -> {
+				int row1 = Math.abs(Utilitaire.indexToRow(((Etat) o1).getCoupPrecedent().getFrom()));
+				int row2 = Math.abs(Utilitaire.indexToRow(((Etat) o2).getCoupPrecedent().getFrom()));
+				int col1 = Math.abs(Utilitaire.indexToColumn(((Etat) o1).getCoupPrecedent().getFrom()) - 4);
+				int col2 = Math.abs(Utilitaire.indexToColumn(((Etat) o2).getCoupPrecedent().getFrom()) - 4);
+				if(row1 == 1 || row1 == 6) {
+					if(row2 != 1 && row2 != 6)
+						return 1;
+				}
+				else {
+					if(row2 == 1 || row2 == 6)
+						return -1;
+				}
+				return col2 == col1 ? 0 : (col2 < col1 ? 1 : -1);
+			});
+		}
 		
 		if(estMax) {
 			int max = Integer.MIN_VALUE;
 			
-			for (Noeud fils : noeud.successeurs()) {
+			for (Noeud fils : succ) {
 				int remonte = alphaBeta(fils, profondeur - 1, false, alpha, beta);
 				if(max < remonte) {
 					max = remonte;
@@ -40,7 +63,7 @@ public class AlphaBeta implements Algorithme{
 		}
 		else {
 			int min = Integer.MAX_VALUE;
-			for (Noeud fils : noeud.successeurs()) {
+			for (Noeud fils : succ) {
 				min = Math.min(min, alphaBeta(fils, profondeur - 1, true, alpha, beta));
 
 				if(min <= alpha)
